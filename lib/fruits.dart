71f5279a2ad64cf6763912c6a15bc18602af2893
jithu2001 <jithu10052001@gmail.com>
2021-10-07 20:40:00 +0530

    prductlist

--- lib/fruits.dart
+++ lib/fruits.dart
@@ -7,28 +7,212 @@ class Fruitpage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      elevation: 0.0,
-      title: RichText(
-        textAlign: TextAlign.left,
-        text: TextSpan(
-            text: "Hi, Customer!",
-            style: TextStyle(fontSize: 20, color: Colors.black),
-            children: <TextSpan>[
-              TextSpan(
-                text: '\nWhat would you buy today?',
-                style: TextStyle(fontSize: 16, color: Colors.grey),
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Image.asset('images/cat/fruit.jpeg'),
+                alignment: Alignment.topCenter,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
-            ]),
-      ),
-      actions: [
-        IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.notifications_outlined,
-              color: Colors.grey,
-            ))
-      ],
-      backgroundColor: Colors.white,
-    ));
+              Container(
+                  height: 120,
+                  width: 350,
+                  margin: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[200]),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: Container(
+                              margin: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.asset(
+                                  'images/products/orange.jpg',
+                                  height: 100,
+                                  width: 2,
+                                  fit: BoxFit.cover,
+                                ),
+                              ))),
+                      Container(
+                        child: RichText(
+                            text: TextSpan(
+                                text: "Fresh oranges\n",
+                                style: TextStyle(
+                                    color: Colors.green, fontSize: 20),
+                                children: <TextSpan>[
+                              TextSpan(
+                                text:
+                                    "\Attributed Lorem is which\nreasonable.injected if type\n",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 15),
+                              )
+                            ])),
+                      ),
+                      Text(
+                        "\$50.0",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
+                      )
+                    ],
+                  )),
+              Container(
+                  height: 120,
+                  width: 350,
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[200]),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: Container(
+                              margin: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.asset(
+                                  'images/products/carrot.jpg',
+                                  height: 100,
+                                  width: 2,
+                                  fit: BoxFit.cover,
+                                ),
+                              ))),
+                      Container(
+                        child: RichText(
+                            text: TextSpan(
+                                text: "Fresh Carrots\n",
+                                style: TextStyle(
+                                    color: Colors.green, fontSize: 20),
+                                children: <TextSpan>[
+                              TextSpan(
+                                text: "\Carrots have calcium and \nvitamins.\n",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 15),
+                              )
+                            ])),
+                      ),
+                      Text(
+                        "\$20.0",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
+                      )
+                    ],
+                  )),
+              Container(
+                  height: 120,
+                  width: 350,
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[200]),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: Container(
+                              margin: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.asset(
+                                  'images/products/grapes.jpg',
+                                  height: 100,
+                                  width: 2,
+                                  fit: BoxFit.cover,
+                                ),
+                              ))),
+                      Container(
+                        child: RichText(
+                            text: TextSpan(
+                                text: "Fresh Juicy Grapes\n",
+                                style: TextStyle(
+                                    color: Colors.green, fontSize: 20),
+                                children: <TextSpan>[
+                              TextSpan(
+                                text:
+                                    "\Packed With Nutrients\n And Vitamins!\n",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 15),
+                              )
+                            ])),
+                      ),
+                      Text(
+                        "\$50.0",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
+                      )
+                    ],
+                  )),
+              Container(
+                  height: 120,
+                  width: 350,
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[200]),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: Container(
+                              margin: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.asset(
+                                  'images/products/broc.jpg',
+                                  height: 100,
+                                  width: 2,
+                                  fit: BoxFit.cover,
+                                ),
+                              ))),
+                      Container(
+                        child: RichText(
+                            text: TextSpan(
+                                text: "Fresh Juicy Grapes\n",
+                                style: TextStyle(
+                                    color: Colors.green, fontSize: 20),
+                                children: <TextSpan>[
+                              TextSpan(
+                                text:
+                                    "\Packed With Nutrients\n And Vitamins!\n",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 15),
+                              )
+                            ])),
+                      ),
+                      Text(
+                        "\$50.0",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
+                      )
+                    ],
+                  )),
+            ],
+          ),
+        ));
   }
 }
